86403bbe4a5180ea9fd642a210dbeec0b0c0f4f9
jdmaradiago2000 <jmaradiago2000@gmail.com>
2022-05-15 12:05:50 -0500

    First Commit

---
+++ models/initModels.js
@@ -0,0 +1,5 @@
+//Establish your models relations inside this function
+
+const initModels = () => {};
+
+module.exports = { initModels };
---
+++ models/restaurantsModels.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('../utils/database');
+
+const Restaurant = db.define('restaurant', {
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  adress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  rating: {
+    type: DataTypes.INTEGER,
+    defaultValue: '5',
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'active',
+  },
+});
+
+module.exports = { Restaurant };
